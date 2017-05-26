479b2d06ef9d36a540093e995891b52fe85813c1
Ariel Elkin <arielelkin@gmail.com>
2017-05-26 15:16:25 -0300

    add php permissions for config

--- run.sh
+++ run.sh
@@ -8,9 +8,14 @@ if [[ ! -d $VOLUME_HOME/mysql ]]; then
     mysql_install_db > /dev/null 2>&1
     echo "=> Done!"
     /create_mysql_admin_user.sh
+
+
 else
     echo "=> Using an existing volume of MySQL"
 fi
+
+chown www-data:www-data -R /var/www/config
+
 
 # Start apache in the background
 service apache2 start
