60e9731047192a852ffbab7028b786919841b560
Ariel Elkin <arielelkin@gmail.com>
2017-05-26 18:10:33 -0300

    resolve deltas

--- run.sh
+++ run.sh
@@ -8,8 +8,6 @@ if [[ ! -d $VOLUME_HOME/mysql ]]; then
     mysql_install_db > /dev/null 2>&1
     echo "=> Done!"
     /create_mysql_admin_user.sh
-
-
 else
     echo "=> Using an existing volume of MySQL"
 fi
