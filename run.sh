541255ffcc44218f663bc8d5b7fd78f9a0871ec5
Eskild Hustvedt <code@zerodogg.org>
2018-06-22 11:39:07 +0200

    Drop overwrite warning

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ fi
 # Update the .dist file in the volume, so that we *KNOW* that it is up-to-date
 # with the ampache version
 sudo -u www-data cp /ampache.cfg.php.dist /var/www/html/config/ampache.cfg.php.dist
-sudo -u www-data perl -pi -E 's{^}{; WARNING: This file will be overwitten during updates. Edit ampache.cfg.php instead\n}' /var/www/html/config/ampache.cfg.php.dist
 
 # Start a process to watch for changes in the library with inotify
 (
